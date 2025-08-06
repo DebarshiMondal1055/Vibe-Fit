b3a97b101dcee2a0dd7166f317de74699b82d49c
DebarshiMondal1055 <rishimondal99@gmail.com>
2025-08-07 01:27:23 +0530

    updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,8 +12,8 @@ app.use(cors({
 }));
 
 
-app.use(express.json({limit:"18kb"}))
-app.use(express.urlencoded({extended:true,limit:"18kb"}));
+app.use(express.json({limit:"50mb"}))
+app.use(express.urlencoded({extended:true,limit:"50mb"}));
 app.use(express.static("public"))
 app.use(cookieParser())
 
